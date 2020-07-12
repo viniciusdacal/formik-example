8da393174960c4ee7ef15cbbe0d7b1ba9f176367
Vinicius Dacal <viniciusldacal@gmail.com>
2020-07-12 16:52:59 -0300

    implement address form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import schema from './schema';
+import { Formik, Field, Form } from 'formik';
 import './App.css';
 
 function App() {
@@ -8,27 +7,72 @@ function App() {
     console.log('SUBMIT', values);
   }
 
+  function onBlurCep(ev, setFieldValue) {
+    const { value } = ev.target;
+
+    const cep = value?.replace(/[^0-9]/g, '');
+
+    if (cep?.length !== 8) {
+      return;
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFieldValue('logradouro', data.logradouro);
+        setFieldValue('bairro', data.bairro);
+        setFieldValue('cidade', data.localidade);
+        setFieldValue('uf', data.uf);
+      });
+  }
+
   return (
     <div className="App">
       <Formik
-        validationSchema={schema}
         onSubmit={onSubmit}
         validateOnMount
         initialValues={{
-          name: '',
-          email: '',
+          cep: '',
+          logradouro: '',
+          numero: '',
+          complemento: '',
+          bairro: '',
+          cidade: '',
+          uf: '',
         }}
-        render={({ values, errors, touched, isValid }) => (
+        render={({ isValid, setFieldValue }) => (
           <Form>
-            <div>
-              <label>Nome</label>
-              <Field name="name" type="text" />
-              <ErrorMessage name="name" />
+            <div className="form-control-group">
+              <label>Cep</label>
+              <Field name="cep" type="text" onBlur={(ev) => onBlurCep(ev, setFieldValue)} />
             </div>
-            <div>
-              <label>Email</label>
-              <Field name="email" type="email" />
-              <ErrorMessage name="email" />
+            <div className="form-control-group">
+              <label>Logradouro</label>
+              <Field name="logradouro" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Número</label>
+              <Field name="numero" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Complemento</label>
+              <Field name="complemento" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>bairro</label>
+              <Field name="bairro" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Cidade</label>
+              <Field name="cidade" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Estado</label>
+              <Field name="uf">
+                <option value={null}>Selecione o Estado</option>
+                <option value="SP">São Paulo</option>
+                <option value="SC">Santa Catarina</option>
+              </Field>
             </div>
             <button type="submit" disabled={!isValid}>Enviar</button>
           </Form>
